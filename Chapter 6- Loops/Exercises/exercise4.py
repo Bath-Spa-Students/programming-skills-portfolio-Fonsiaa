a31cb733ae24af15bf1ff6e8281e4fb873107d5c
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 20:06:18 +0400

    exercise4.py

--- Chapter 6- Loops/Exercises/exercise4.py
+++ Chapter 6- Loops/Exercises/exercise4.py
@@ -1,12 +1,10 @@
 sandwich_orders = ['Cucumber tea', 'Grilled cheese', 'Mayonnaise tuna'] #list
 finished_sandwiches = []
-
 #using the f-string
 while sandwich_orders:
-    current_sandwich = sandwich_orders.pop()
+    current_sandwich = sandwich_orders.pop()#removing a sandwich
     print(f"I'm still making your {current_sandwich} sandwich.")
     finished_sandwiches.append(current_sandwich) #prints the finished_sandwiches list
-
 #using the f-string
 print("\n")
 for sandwich in finished_sandwiches:
