ce8d1083cd895fbaf5c79cd67456d66ca650201f
Fonsiaa <abeaalfonso@gmail.com>
2023-11-15 19:29:33 +0400

    Class_practice3.py

---
+++ Class Practice/#Q3.py
@@ -0,0 +1,14 @@
+#Q3 You need to make a program for a leaderboard. 
+#The program needs to output the number 1 to 9, each on a separate line 
+#followed by a dot
+#creating multiply lines
+print (''' Leaderboard:
+    1.
+    2.
+    3.
+    4.
+    5.
+    6.
+    7.
+    8.
+    9. ''')#prints the numbers in separated line
