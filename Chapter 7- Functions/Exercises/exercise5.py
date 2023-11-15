cabc105b7343116095e52555e721806c0f8342e5
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 20:14:36 +0400

    exercise5.py

--- Chapter 7- Functions/Exercises/exercise5.py
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -1,13 +1,12 @@
 def city_country(city, country):
     """Return a string like 'Santiago, Chile'.""" #commenting
     #using the f-string
-    return f"{city.title()}, {country.title()}" #function ends
-
+    return f"{city.title()}, {country.title()}"
 city = city_country('Copenhagen', 'Denmark')
 print(city)#prints the values
 
 city = city_country('Toronto', 'Canada')
-print(city)
+print(city)#assigning the value to the caller
 
 city = city_country('Singapore', 'Republic of Singapore')
 print(city)
