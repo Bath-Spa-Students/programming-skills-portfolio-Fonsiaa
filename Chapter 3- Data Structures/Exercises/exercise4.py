c92fd2181d09a5670de5a9bd9df409d9f3bce36c
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:45:36 +0400

    exercise4.py

--- Chapter 3- Data Structures/Exercises/exercise4.py
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -1,9 +1,6 @@
 #Uses the positive indexing
 #g = guest
-g = ("Sheila" , "Juluis" , "Charles")
-#Sheila = 0
-#Juluis = 1
-#Charles = 2
+g = ("Sheila" , "Juluis" , "Tobi")
 
 #string format () formula
 print ("Good day {}, I would like to invite you to dinner.".format(g[0]))
