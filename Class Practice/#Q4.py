c6a03a5ff22d86135120bb0ce0961224e8ae0200
Fonsiaa <abeaalfonso@gmail.com>
2023-11-15 19:29:47 +0400

    Class_practice4.py

---
+++ Class Practice/#Q4.py
@@ -0,0 +1,8 @@
+#Q4: Compute the area of a triangle
+
+import math #python access another module
+base = float(input("Input a number for your triangle's base:"))#asks user to input a number
+height = float(input("Input a number for your triangle's height:"))#asks user to input a number
+#formula to compute for the area of the triangle
+area = 0.5 * base * height #calculates the input from the user
+print("The area of your triangle is", area) #print the calculation of the triangle
