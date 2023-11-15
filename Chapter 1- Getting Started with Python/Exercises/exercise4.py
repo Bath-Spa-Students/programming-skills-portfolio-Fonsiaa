efa4f3d9274dbf331017797c9b7b4bb05a041c2a
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:35:36 +0400

    exercise4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -1,4 +1,5 @@
+#putting 1 value per variable
 string1= "Hello"
 string2= "Other"
 string3= "World"
-print(string1, string2, string3)
+print(string1, string2, string3)#prints the value of the variables in one line
