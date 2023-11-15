6085bf1b6b0871972b3e577956b80d90f72627b8
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:59:58 +0400

    exercise1.py

--- Chapter 6- Loops/Exercises/exercise1.py
+++ Chapter 6- Loops/Exercises/exercise1.py
@@ -1,9 +1,9 @@
-message = "\nWhat more toppings on your pizza? or enter 'quit': "
+message = "\nWhat more toppings do you want on your pizza? or 'no': "
 
 #using the if-else statement
 while True:
     topping = input(message)
-    if topping != 'quit':
+    if topping != 'no':
         print(f"  I'll add {topping} to your pizza.")
     else:
         break #will break the loop
