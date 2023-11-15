38914ff6353e46049cbeb0fa55f48abf86761b4a
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:48:57 +0400

    Delete Chapter 4- Control Flow/Exercises/exercise2.1.py

--- Chapter 4- Control Flow/Exercises/exercise2.1.py
+++
@@ -1,9 +0,0 @@
-#variable is red
-alien_colour = "green"
-
-if alien_colour == "red":
-#pts is points
-    print ("Because of the you just earned 5pts")
-else:
-#pts is point
-    print("Because of the you just earned 10pts")
