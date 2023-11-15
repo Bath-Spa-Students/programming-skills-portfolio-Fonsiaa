44bfaadcd785f19f36e1f1db228cddc136dc15ad
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:39:55 +0400

    exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise5.py
@@ -0,0 +1,8 @@
+print (int(50/6))#number will be an integer
+
+print (float(50-48))#number will be a float
+
+#using f-string
+get = "8"
+change = "2"
+print (f'She can {get} USB, and her change is £{change}.')
