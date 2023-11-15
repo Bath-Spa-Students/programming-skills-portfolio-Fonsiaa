d4b3287dd6c8f06df39e17ee1d12bdad399734e9
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:33:00 +0400

    exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -1,6 +1,7 @@
-print (''' Twinkle, twinkle, little star,
+#creating a multi-line string
+print (''' Twinkle, twinkle, little star, 
     How I wander what you are!
             Up above the world so high,
             Like a diamond in the sky.
 Twinkle, twinkle, little star,
-    How I wander what you are''')
+    How I wander what you are''') #triple single quotation
