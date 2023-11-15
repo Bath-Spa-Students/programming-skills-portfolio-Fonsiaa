e178f43432d2e0878c3d7813893c66b6d17f6485
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 20:07:38 +0400

    exercise1.py

--- Chapter 7- Functions/Exercises/exercise1.py
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -3,5 +3,4 @@ def display_message(): #the function
     ''' What I'm learning''' #similar to commenting or using "#"
     msg = "I've learn to store a code."
     print(msg) #prints only the string value
-
 display_message() 
