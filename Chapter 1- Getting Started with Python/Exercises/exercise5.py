9524b532964df5d09a6714430bb9638fff189232
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:35:59 +0400

    exercise5.py

--- Chapter 1- Getting Started with Python/Exercises/exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise5.py
@@ -1,5 +1,5 @@
 import math
-radius=float(input("Enter the radius of the circle: "))
+radius=float(input("Enter the radius of the circle: "))#collects an input from user
 # formula for the circle
-area = math.pi * (radius ** 2)
-print ("Area is:", area)
+area = math.pi * (radius ** 2) #calculates the area of the triangle
+print ("Area is:", area)#print the area as a float
