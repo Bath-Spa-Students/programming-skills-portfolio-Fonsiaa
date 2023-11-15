9f1f01d5a767d66a656683c2de31c1f2c3413c01
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:41:11 +0400

    exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+#making a list
+names = ["Pl", "Eli" , "Zoro" , "Erikson" , "Seb", "Belle"] 
+#prints all of the names
+print (names[0])
+print (names[1])
+print (names[2])
+print (names[3])
+print (names[4])
+print (names[5])
