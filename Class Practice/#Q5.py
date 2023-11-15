f4ffff5ce8250654dfccc09b95e4e9963eee6e28
Fonsiaa <abeaalfonso@gmail.com>
2023-11-15 19:30:04 +0400

    Class_practice5.py

---
+++ Class Practice/#Q5.py
@@ -0,0 +1,7 @@
+#Q5: Write four strings in different variables and print the output 
+#as one string (String Concetenation)
+string_1 = "Hi, I'am lola"
+string_2 = "and I'm"
+string_3 = "12"
+string_4 = "years old."
+print (string_1  + string_2  + string_3 +  string_4)
