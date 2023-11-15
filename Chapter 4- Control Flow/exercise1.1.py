eea13dbde523ae8e2c4d33d5b0fe88d6a37b5dcd
beatricee <144213677+Fonsiaa@users.noreply.github.com>
2023-11-15 19:52:06 +0400

    Add files via upload

---
+++ Chapter 4- Control Flow/exercise1.1.py
@@ -0,0 +1,5 @@
+#variable is red
+alien_colour = "red"
+
+if alien_colour == "green":
+    print ("Earned 5 points")
---
+++ Chapter 4- Control Flow/exercise2.1.py
@@ -0,0 +1,9 @@
+#variable is red
+alien_colour = "green"
+
+if alien_colour == "red":
+#pts is points
+    print ("Because of the you just earned 5pts")
+else:
+#pts is point
+    print("Because of the you just earned 10pts")
